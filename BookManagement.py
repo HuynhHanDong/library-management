302e1f2867964e3db1afd7898934aa093cb6c638
Ashton <huynhhandong@gmail.com>
2024-04-25 16:55:23 +0700

    Added some functions associated to borrowed_book list

--- BookManagement.py
+++ BookManagement.py
@@ -8,20 +8,10 @@ class Book:
         self.borrowed = 0
         self.status = "Available"
 
-    def borrow_book(self):
-        if self.borrowed != self.quantity:
-            self.borrowed += 1
-        if self.borrowed == self.quantity:
-            self.status = "Not Available"
-
-    def return_book(self):
-        self.borrowed -= 1
-        if self.borrowed < self.quantity:
-            self.status = "Available"
-
 class BooksManager:
     def __init__(self):
         self.book_list = dict()
+        self.borrowed_book = list()
 
     def add_book(self, book):
         self.book_list[book.id] = [book.title, book.author, book.genre, book.quantity, book.borrowed, book.status]
@@ -30,14 +20,13 @@ class BooksManager:
     def change_info(self):
         change = input("Enter book's id to change info: ")
         if change in self.book_list:
-            num = int(input(
-                "Choose the number of category you want to change: 1.title  |  2.author  |  3.genre  |  4.quantity \n"))
+            num = int(input("Choose the number of category you want to change: 1.title  |  2.author  |  3.genre  |  4.quantity \n"))
             if 0 < num < 5:
                 new = input("Enter new info: ")
                 self.book_list[change][num - 1] = new.title()
                 print("Book info changed!")
             else:
-                print("Please choose a number between 1 and 4. Try again!")
+                print("Invalid number. Please try again!")
         else:
             print("Book no found.")
 
@@ -60,47 +49,111 @@ class BooksManager:
             print("Book not found.")
 
     def display_book_list(self):
-        print_table()
-        for book in sorted(self.book_list):
-            print(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|{self.book_list[book][2].center(15)}|"
-                  f"{str(self.book_list[book][3]).center(15)}|{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|")
-            print("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+")
+        if not self.book_list:
+            print("There is no book to display.")
+        else:
+            print_table()
+            for book in sorted(self.book_list):
+                print(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|"
+                      f"{self.book_list[book][2].center(15)}|{str(self.book_list[book][3]).center(15)}|"
+                      f"{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|")
+                print("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+")
 
     def sort_book_list(self):  # Sort book title
-        print_table()
-        for book, value in sorted(self.book_list.items(), key=lambda info: info[1][0]):
-            print(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|{self.book_list[book][2].center(15)}|"
-                  f"{str(self.book_list[book][3]).center(15)}|{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|")
-            print("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+")
+        if not self.book_list:
+            print("There is no book to sort.")
+        else:
+            print_table()
+            for book, value in sorted(self.book_list.items(), key=lambda info: info[1][0]):
+                print(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|"
+                      f"{self.book_list[book][2].center(15)}|{str(self.book_list[book][3]).center(15)}|"
+                      f"{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|")
+                print("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+")
 
     def filter(self):
-        category = int(input("Choose a number of category to filter: 1.title  |  2.author  |  3.genre  |  4.status \n"))
-        if 0 < category < 5:
-            if category == 4:
-                category += 2  # change index to Status
-                value = input("Enter a value to filter out:  Available  |  Not Available \n")
-            else:
-                value = input("Enter the value to filter out: ")
-            print_table()
-            for book in self.book_list:
-                if self.book_list[book][category - 1] == value.title():
-                    print(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|{self.book_list[book][2].center(15)}|"
-                          f"{str(self.book_list[book][3]).center(15)}|{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|")
-                    print("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+")
+        if not self.book_list:
+            print("There is no book to filter.")
         else:
-            print("Please choose a number between 1 and 5. Try again!")
+            category = int(
+                input("Choose a number of category to filter: 1.title  |  2.author  |  3.genre  |  4.status \n"))
+            if 0 < category < 5:
+                if category == 4:
+                    category += 2  # change index to Status
+                    value = input("Enter a value to filter out:  Available  |  Not Available \n")
+                else:
+                    value = input("Enter the value to filter out: ")
+                print_table()
+                for book in self.book_list:
+                    if self.book_list[book][category - 1] == value.title():
+                        print(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|"
+                              f"{self.book_list[book][2].center(15)}|{str(self.book_list[book][3]).center(15)}|"
+                              f"{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|")
+                        print("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+")
+            else:
+                print("Invalid number. Please try again!")
 
-    def save_file(self):
-        data = open("Book_list.txt", "w")
-        data.write("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+\n")
-        data.write("|  ID    |        Title        |       Author       |     Genre     |   Quantity    |   Borrowed    |    Status     |\n")
-        data.write("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+\n")
-        for book in sorted(self.book_list):
-            data.write(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|{self.book_list[book][2].center(15)}|"
-                       f"{str(self.book_list[book][3]).center(15)}|{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|\n")
+    def borrow_book(self, id):
+        if self.book_list[id][5] == "Available":
+            self.borrowed_book.append(id)
+            self.book_list[id][4] += 1
+            print("Borrowed book successfully!")
+        else:
+            print("This book is not available now!")
+        if self.book_list[id][3] == self.book_list[id][4]:
+            self.book_list[id][5] = "Not Available"
+
+    def return_book(self, id):
+        try:
+            self.borrowed_book.remove(id)
+            self.book_list[id][4] -= 1
+            print("Returned book successfully!")
+            if self.book_list[id][4] < self.book_list[id][3]:
+                self.book_list[id][5] = "Available"
+        except:
+            print("This book was not borrowed.")
+
+    def print_borrowed_list(self):
+        if not self.borrowed_book:
+            print("There is no borrowed book to display.")
+        else:
+            print("+------------------------------+")
+            print("|        Borrowed Books        |")
+            print("+--------+---------------------+")
+            print("|  ID    |        Title        |")
+            print("+--------+---------------------+")
+            for book in self.borrowed_book:
+                print(f'|{book.center(8)}|{self.book_list[book][0].center(21)}|')
+                print("+--------+---------------------+")
+
+    def save_file_borrowed_book(self):
+        if not self.borrowed_book:
+            print("There is no data to save.")
+        else:
+            data = open("Borrowed_books.txt", "w")
+            data.write("+------------------------------+\n")
+            data.write("|        Borrowed Books        |\n")
+            data.write("+--------+---------------------+\n")
+            data.write("|  ID    |        Title        |\n")
+            data.write("+--------+---------------------+\n")
+            for book in self.borrowed_book:
+                data.write(f'|{book.center(8)}|{self.book_list[book][0].center(21)}|\n')
+                data.write("+--------+---------------------+\n")
+
+    def save_file_book_list(self):
+        if not self.book_list:
+            print("There is no book data to save.")
+        else:
+            data = open("Book_list.txt", "w")
             data.write("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+\n")
-        print("Saved to file!")
-        data.close()
+            data.write("|  ID    |        Title        |       Author       |     Genre     |   Quantity    |   Borrowed    |    Status     |\n")
+            data.write("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+\n")
+            for book in sorted(self.book_list):
+                data.write(f"|{book.center(8)}|{self.book_list[book][0].center(21)}|{self.book_list[book][1].center(20)}|"
+                           f"{self.book_list[book][2].center(15)}|{str(self.book_list[book][3]).center(15)}|"
+                           f"{str(self.book_list[book][4]).center(15)}|{self.book_list[book][5].center(15)}|\n")
+                data.write("+--------+---------------------+--------------------+---------------+---------------+---------------+---------------+\n")
+            print("Saved to file!")
+            data.close()
 
     def check_id(self, id):
         if id in self.book_list:
--- Main.py
+++ Main.py
@@ -1,16 +1,44 @@
 from BookManagement import*
 
 def main():
-    manager = BooksManager()
-
     while True:
-        print("\n+------------ Student Management System ------------+")
+        print("\n+------------ Library Management System ------------+")
+        print("|                                                   |")
+        print("|  Which Section Would You Like To Manage?          |")
+        print("|  1. Book Management                               |")
+        print("|  2. Reader Management                             |")
+        print("|  3. Exit                                          |")
+        print("|                                                   |")
+        print("+---------------------------------------------------+")
+
+        try:
+            choice = int(input("Enter the number of your choice: "))
+            if choice == 1:
+                print("Moving to Book Management Section...")
+                book_menu()
+            elif choice == 2:
+                print("Moving to Reader Management Section...")
+                pass
+            elif choice == 3:
+                print("Exiting program...")
+                break
+            else:
+                print("Invalid choice. Please try again!")
+        except:
+            print("Invalid input. Please try again!")
+        print()
+        print("~" * 120)
+
+def book_menu():
+    manager = BooksManager()
+    while True:
+        print("\n+----------------- Book Management -----------------+")
         print("|  1. Add Book                                      |")
         print("|  2. Change Book Info                              |")
         print("|  3. Remove Book                                   |")
         print("|  4. Find Book                                     |")
         print("|  5. Display All Books                             |")
-        print("|  6. Print Book list By Alphabetical Title         |")
+        print("|  6. Sort Book list By Alphabetical Title          |")
         print("|  7. Display Book List With filter                 |")
         print("|  8. Save Data To File                             |")
         print("|  9. Exit                                          |")
@@ -21,7 +49,7 @@ def main():
             if choice == 1:
                 id = input("Enter book's id: ")
                 if manager.check_id(id):
-                    print("This ID is already existed.")
+                    print("This ID is already existed. Please try again!")
                     continue
                 title = input("Enter book's title: ")
                 title = title.title()
@@ -31,7 +59,7 @@ def main():
                 genre = genre.title()
                 quantity = int(input("Enter number of copies: "))
                 if quantity <= 0:
-                    print("Number of copies must be larger than 0. ")
+                    print("Number of copies must be larger than 0. Please try again!")
                     continue
                 book = Book(id, title, author, genre, quantity)
                 manager.add_book(book)
@@ -55,16 +83,22 @@ def main():
                 manager.filter()
 
             elif choice == 8:
-                manager.save_file()
+                manager.print_borrowed_list()
 
             elif choice == 9:
-                print("Exiting program...")
+                manager.save_file_borrowed_book()
+
+            elif choice == 10:
+                manager.save_file_book_list()
+
+            elif choice == 11:
+                print("Exiting Book Management Section...")
                 break
 
             else:
                 print("Invalid choice. Please try again.")
         except:
-            print("Invalid input. Try again.")
+            print("Invalid input. Please try again.")
         print()
         print("~" * 120)
 
